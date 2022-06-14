77f5387ac58803a7cfbd5bea1b9fc9c63377bc3a
erwin antonius <erwin.ant@gmail.com>
2022-06-14 14:13:15 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,28 +4,28 @@ module.exports = {
       script: "dist/main.js",
       env: {
         NODE_ENV: "dev",
-        APP_URL:"http://localhost:3000",
+        APP_URL:"http://localhost:8080",
         WEBTOKEN_SECRET_KEY:"uAsBw6WxqD",
         WEBTOKEN_EXPIRATION_TIME:2400,
         DB_URL:"mongodb+srv://muf:Cideng123@cluster0.xzsj6.mongodb.net/fdedb?retryWrites=true&w=majority"
       },
       env_test: {
         NODE_ENV: "dev",
-        APP_URL:"http://localhost:3000",
+        APP_URL:"http://localhost:8080",
         WEBTOKEN_SECRET_KEY:"uAsBw6WxqD",
         WEBTOKEN_EXPIRATION_TIME:2400,
         DB_URL:"mongodb+srv://muf:Cideng123@cluster0.xzsj6.mongodb.net/fdedb?retryWrites=true&w=majority"
       },
       env_staging: {
         NODE_ENV: "dev",
-        APP_URL:"http://localhost:3000",
+        APP_URL:"http://localhost:8080",
         WEBTOKEN_SECRET_KEY:"uAsBw6WxqD",
         WEBTOKEN_EXPIRATION_TIME:2400,
         DB_URL:"mongodb+srv://muf:Cideng123@cluster0.xzsj6.mongodb.net/fdedb?retryWrites=true&w=majority"
       },
       env_production: {
         NODE_ENV: "dev",
-        APP_URL:"http://localhost:3000",
+        APP_URL:"http://localhost:8080",
         WEBTOKEN_SECRET_KEY:"uAsBw6WxqD",
         WEBTOKEN_EXPIRATION_TIME:2400,
         DB_URL:"mongodb+srv://muf:Cideng123@cluster0.xzsj6.mongodb.net/fdedb?retryWrites=true&w=majority"
